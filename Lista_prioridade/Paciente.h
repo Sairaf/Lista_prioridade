fe5b09941b357de16cd2094e0eae69655492eace
Sairaf <abnergillet16@gmail.com>
2014-11-21 01:08:11 -0200

    fasf

--- Lista_prioridade/Paciente.h
+++ Lista_prioridade/Paciente.h
@@ -1,12 +1,7 @@
-/* 
- * File:   Paciente.h
- * Author: ABGerson
- *
- * Created on 20 de Novembro de 2014, 23:08
- */
 
 #ifndef PACIENTE_H
 #define	PACIENTE_H
+#define PACIENTE_H
 #define <iostream>
 #define <cstdlib>
 #define <string>
@@ -15,45 +10,45 @@ using namespace std;
 
 
 class Paciente {
-    
-    
+
+
     friend ostream &operator<<(ostream&, const Paciente&);
 public:
     Paciente();
     Paciente(const Paciente& orig);
     virtual ~Paciente();
-    
+
     string getNome(){
-      return this->nome;  
+      return this->nome;
     }
-    
+
     string getTelefone(){
-     return this->telefone;   
+     return this->telefone;
     }
-    
+
     int getPrioridade(){
      return this->num_Prioridade;
     }
-    
+
     void setNome(const string& nome){
-        this.nome = nome;
+        this->nome = nome;
     }
-    
+
     void setTelefone(const string& telefone){
         this->telefone = telefone;
     }
-    
+
     void setPrioridade(const int& priori){
         this->num_Prioridade = priori;
     }
-    
+
     Paciente operator&(const Paciente&);
-    
+
 protected:
     string nome;
     int num_Prioridade;
     string telefone;
-    
+
 };
 
 #endif	/* PACIENTE_H */
--- Lista_prioridade/main.cpp
+++ Lista_prioridade/main.cpp
@@ -1,57 +1,72 @@
-/* 
+/*
  * File:   main.cpp
  * Author: ABGerson
  *
  * Created on 20 de Novembro de 2014, 23:07
  */
-
+#ifndef MAIN_H
+#define MAIN_H
+#endif // MAIN_H
 #include <cstdlib>
 #include <stdio.h>
 #include <string>
 #include <vector>
 #include "Paciente.h"
-
+#define MAX 10
 using namespace std;
 
 /*
- * 
+ *
  */
 int main(int argc, char** argv) {
 
+    Paciente lista[2*MAX+1];
     int tamanho_Lista;
     return 0;
 }
 
 void Subir(Paciente * vetor[],int pos){
- Paciente aux;
+ Paciente* aux;
  int pos_pai = pos/2;
  if(pos_pai >= 1){
   if(vetor[pos] > vetor[pos_pai])   {
      aux = vetor[pos] ;
      vetor[pos] = vetor[pos_pai];
      vetor[pos_pai] = aux;
-     Subir(vetor, pos_pai);
   }
+  Subir(vetor, pos_pai);
  }
 }
 
 
 void Descer(Paciente * vetor[], int pos, int MAX){
-    int pos_filho = pos *2, aux;
+    int pos_filho = pos *2;
+    Paciente* aux;
     if(pos_filho < MAX){
       if(pos_filho <= MAX){
           if(vetor[pos_filho+1] > vetor[pos_filho])
               pos_filho++;
           if(vetor[pos]->getPrioridade() < vetor[pos_filho]->getPrioridade()){
               aux = vetor[pos] ;
-              
-              vetor[pos] = vetor[pos_filho];                                           
-              vetor[pos_filho]'' = aux;
-              Subir(vetor, pos_filho, MAX);
+
+              vetor[pos] = vetor[pos_filho];
+              vetor[pos_filho] = aux;
+
           }
-          
-          
-    }  
+
+
+    }
+     Subir(vetor, pos_filho, MAX);
    }
-}  
+}
+/*
+void Escrever(Paciente* vector[], int tamanho){
+ int i;
+ for(i = 0; i < tamanho;i++){
+ cout << "teste";
+ }
+
+}
+
+*/
 
